f5d3d29d3800f51e37d5d58a892a5e8be823df4d
Andrea Camacho <acamacho176@unab.edu.co>
2020-10-10 15:19:00 -0500

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
+import { AppComponent } from './app.component';
 
 import "hammerjs";
 import { MenuComponent } from './menu/menu.component';
@@ -48,7 +53,11 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     FlexLayoutModule,
     AppRoutingModule,
-    MatDialogModule
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    FormsModule
 
   ],
   providers: [
